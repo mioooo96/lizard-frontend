769b749f0344f10964286566ff00e3ec1178dcdc
new mod <3266023504@qq.com>
2025-05-11 16:23:39 +0800

    '修改v4'

--- src/components/PagesTemplate.vue
+++ src/components/PagesTemplate.vue
@@ -100,13 +100,11 @@
   import { ref,computed,onMounted,onUnmounted,onBeforeMount } from 'vue'
   import {useRouter,useRoute} from 'vue-router'
   import {type ProductInter,type Products} from '@/types'
-  import { useProductsStore } from '@/store/Products'
   import axios from 'axios'
   import { toast } from 'vue3-toastify';
   import 'vue3-toastify/dist/index.css';
 
   const Props = defineProps(['Ptype'])
-  const ProductsStore = useProductsStore()
   //const products = ProductsStore.getProductsByPtype(Props.Ptype)
   const searchKeyword = ref('')
   const isLoggedIn = ref(false)
--- src/components/SearchTemplate.vue
+++ src/components/SearchTemplate.vue
@@ -103,14 +103,12 @@
   import { ref,computed,onMounted,onUnmounted, watch,onBeforeMount } from 'vue'
   import {useRouter,useRoute} from 'vue-router'
   import {type ProductInter,type Products} from '@/types'
-  import { useProductsStore } from '@/store/Products'
   import axios from 'axios'
   import { toast } from 'vue3-toastify';
   import 'vue3-toastify/dist/index.css';
 
   const Props = defineProps(['keysearch'])
   console.log(Props.keysearch)
-  const ProductsStore = useProductsStore()
   //const products = ProductsStore.getProductsByPtype(Props.Ptype)
   const searchKeyword = ref('')
   const isLoggedIn = ref(false)
